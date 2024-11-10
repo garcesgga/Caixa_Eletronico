83c1312c0f3f21c72aeb217ee0b7827dfbf26d81
garces <guilherme.almada@uvvnet.com.br>
2024-11-10 13:37:41 -0300

    Adicionar arquivos de projeto.

---
+++ caixaeletronico/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace caixaeletronico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDeposito = new System.Windows.Forms.Button();
+            this.btnSaque = new System.Windows.Forms.Button();
+            this.btnExtrato = new System.Windows.Forms.Button();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDeposito
+            // 
+            this.btnDeposito.AutoSize = true;
+            this.btnDeposito.Font = new System.Drawing.Font("Calibri", 15F);
+            this.btnDeposito.Location = new System.Drawing.Point(309, 281);
+            this.btnDeposito.Name = "btnDeposito";
+            this.btnDeposito.Size = new System.Drawing.Size(115, 52);
+            this.btnDeposito.TabIndex = 0;
+            this.btnDeposito.Text = "Depositar";
+            this.btnDeposito.UseVisualStyleBackColor = true;
+            this.btnDeposito.Click += new System.EventHandler(this.btnDeposito_Click);
+            // 
+            // btnSaque
+            // 
+            this.btnSaque.AutoSize = true;
+            this.btnSaque.Font = new System.Drawing.Font("Calibri", 15F);
+            this.btnSaque.Location = new System.Drawing.Point(309, 213);
+            this.btnSaque.Name = "btnSaque";
+            this.btnSaque.Size = new System.Drawing.Size(115, 52);
+            this.btnSaque.TabIndex = 1;
+            this.btnSaque.Text = "Sacar";
+            this.btnSaque.UseVisualStyleBackColor = true;
+            this.btnSaque.Click += new System.EventHandler(this.btnSaque_Click);
+            // 
+            // btnExtrato
+            // 
+            this.btnExtrato.AutoSize = true;
+            this.btnExtrato.Font = new System.Drawing.Font("Calibri", 15F);
+            this.btnExtrato.Location = new System.Drawing.Point(309, 142);
+            this.btnExtrato.Name = "btnExtrato";
+            this.btnExtrato.Size = new System.Drawing.Size(115, 52);
+            this.btnExtrato.TabIndex = 2;
+            this.btnExtrato.Text = "Extrato";
+            this.btnExtrato.UseVisualStyleBackColor = true;
+            this.btnExtrato.Click += new System.EventHandler(this.btnExtrato_Click);
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(303, 32);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(74, 33);
+            this.lblSaldo.TabIndex = 3;
+            this.lblSaldo.Text = "Saldo";
+            this.lblSaldo.Click += new System.EventHandler(this.lblSaldo_Click);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(324, 92);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.btnExtrato);
+            this.Controls.Add(this.btnSaque);
+            this.Controls.Add(this.btnDeposito);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDeposito;
+        private System.Windows.Forms.Button btnSaque;
+        private System.Windows.Forms.Button btnExtrato;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.TextBox txtValor;
+    }
+}
+
---
+++ caixaeletronico/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/*Crie um programa em C# com Windows Forms que simule um caixa eletrônico simples. O programa deve ter:
+Um formulário principal com um campo para exibir o saldo da conta.
+Botões para as operações de depósito, saque e extrato.
+Ao clicar em "Depósito", o programa deve solicitar o valor do depósito e atualizar o saldo.
+Ao clicar em "Saque", o programa deve solicitar o valor do saque, verificar se há saldo suficiente e atualizar o saldo.
+Ao clicar em "Extrato", o programa deve exibir um histórico das operações realizadas (depósitos e saques).
+Utilize uma List<T> para armazenar o histórico das operações.
+Utilize exceções para tratar erros como saldo insuficiente ou valor inválido.*/
+
+
+namespace caixaeletronico
+{
+    public partial class Form1 : Form
+    {
+        private double saldo = 1000.00;
+        private List<string> extrato = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            AtualizarSaldo();
+        }
+
+        private void btnDeposito_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double valor = double.Parse(txtValor.Text);
+                if (valor <= 0)
+                {
+                    throw new ArgumentException("O valor deve ser maior do que zero!");
+                }
+                saldo += valor;
+                extrato.Add($"Depósito de R$ {valor:C}");
+                AtualizarSaldo();
+                LimparCampoValor();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Digite um valor válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSaque_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double valor = double.Parse(txtValor.Text);
+                if (valor <= 0)
+                {
+                    throw new ArgumentException("O valor deve ser maior do que zero!");
+                }
+                if (valor > saldo)
+                {
+                    throw new InvalidOperationException("Saldo insuficiente!");
+                }
+                saldo -= valor;
+                extrato.Add($"Saque: -{valor:C}");
+                AtualizarSaldo();
+                LimparCampoValor();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Digite um valor válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void btnExtrato_Click(object sender, EventArgs e)
+        {
+            string historico = "Extrato:\n";
+            foreach(string operacao in extrato)
+            {
+                historico += operacao + "\n";
+            }
+            MessageBox.Show(historico, "Extrato", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void AtualizarSaldo()
+        {
+            lblSaldo.Text =$"Saldo: {saldo:C}";
+        }
+
+        private void lblSaldo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LimparCampoValor()
+        {
+            txtValor.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
